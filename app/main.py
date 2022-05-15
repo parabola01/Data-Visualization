c5f7bd7b043d1bab993a5a9a78bc0287a6fd9ced
Strus01 <maciek.struski@gmail.com>
2022-05-15 18:20:24 +0200

    heatmapy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-import datetime
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import datetime
 import os
 
 st.title('Wizualizacja projekt')
@@ -20,6 +22,21 @@ def read_files():
     return netflix_top10, netflix_data, netfix_countries
 
 
+def top_10_all_time():
+    df1 = df_top10.copy()
+    df1.sort_values(by='Days In Top 10', ascending=False, inplace=True)
+    df1['Title'] = df1['Title'].drop_duplicates(keep='first')
+    df1 = df1.dropna()
+    return df1
+
+
+def split_file(df, col):
+    df_movies = df[df[col] == 'Movie']
+    df_series = df[df[col] == 'TV Show']
+
+    return df_movies, df_series
+
+
 df_top10, df_titles, df_countries = read_files()
 
 st.write('Który dzień chcesz zobaczyć?')
@@ -34,31 +51,13 @@ for i, title in enumerate(ranking):
     st.write(i + 1, title)
 
 
-def top_10_all_time():
-    df1 = df_top10.copy()
-    df1.sort_values(by='Days In Top 10', ascending=False, inplace=True)
-    df1['Title'] = df1['Title'].drop_duplicates(keep='first')
-    df1 = df1.dropna()
-    return df1
-
-
-def split_file(df, col):
-    df_movies = df[df[col] == 'Movie']
-    df_movies = df_movies.iloc[:10]
-
-    df_series = df[df[col] == 'TV Show']
-    df_series = df_series.iloc[:10]
-
-    return df_movies, df_series
-
-
 df_top10_all_time = top_10_all_time()
 df_top10_all_time_movies, df_top10_all_time_series = split_file(df_top10_all_time, 'Type')
 
-fig = px.bar(df_top10_all_time_movies, x='Title', y='Days In Top 10')
+fig = px.bar(df_top10_all_time_movies.iloc[:10], x='Title', y='Days In Top 10')
 st.plotly_chart(fig)
 
-fig = px.bar(df_top10_all_time_series, x='Title', y='Days In Top 10')
+fig = px.bar(df_top10_all_time_series.iloc[:10], x='Title', y='Days In Top 10')
 st.plotly_chart(fig)
 
 # czego ludzie więcej oglądają czy netflix exclusive czy nie exclusive
@@ -82,8 +81,35 @@ fig = px.choropleth(
     locations='iso_alpha',
     color='Count',
     color_continuous_scale=px.colors.sequential.Reds,
-    width=1000,
+    width=800,
     height=800
 )
 st.plotly_chart(fig)
 
+df_titles['genre'] = df_titles['listed_in'].apply(lambda x :  x.replace(' ,',',').replace(', ',',').split(','))
+df_titles_movies, df_titles_series = split_file(df_titles, 'type')
+res_movies = df_titles_movies['genre'].str.join('|').str.get_dummies()
+res_series = df_titles_series['genre'].str.join('|').str.get_dummies()
+
+
+def corr_heatmap(df):
+    corr = df.corr()
+    heat = go.Heatmap(z=corr, x=corr.columns.values, y=corr.columns.values)
+    layout = go.Layout(width=800, height=800)
+    figure = go.Figure(data=heat, layout=layout)
+    return figure
+
+
+def calc_no_of_types(df):
+    types = []
+    for i in df['genre']: types += i
+    types = set(types)
+    return len(types)
+
+
+fig = corr_heatmap(res_movies)
+st.plotly_chart(fig)
+
+
+fig = corr_heatmap(res_series)
+st.plotly_chart(fig)
